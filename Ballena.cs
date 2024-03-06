6147429201c21d1aa9671931829dec9017d42e28
Gabriel55103 <161339529+Gabriel55103@users.noreply.github.com>
2024-03-06 08:35:35 -0600

    Add files via upload

---
+++ Ballena.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Ballena : Mamifero
+    {
+        //Constructor
+        public Ballena(string n, double p): base(n, p)
+        {
+        }
+
+        //Metodos 
+
+        public void mostrarballena()
+        {
+            string nom = "";
+            nom = getNombre();
+            Console.WriteLine("El nombre es: "+ nom);
+            Console.WriteLine("El peso de la ballena es: " + peso + "Kg");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Variables 
+using Herencia;
+
+string nombre;
+double peso;
+int nBigotes;
+
+
+//Ingresar y mostrar clase gato 
+Console.WriteLine("------Clase gato------"); 
+Console.Write("Ingresa el nombre del gato: ");
+nombre = Console.ReadLine();
+
+Console.Write("Ingresa el peso: ");
+peso = double.Parse(Console.ReadLine());
+
+Console.Write("Ingresa el numero de bigotes: ");
+nBigotes = int.Parse(Console.ReadLine());
+
+//Instancia de los objetos 
+//Gato 
+Gato gato1 = new Gato(nombre, peso, nBigotes);
+
+Console.WriteLine();
+gato1.mostrarGato();
+
+//Cambiando el nombre al gato
+gato1.setNombre("Lucifer");
+nombre = gato1.getNombre();
+Console.WriteLine("\n---Le cambie el nombre al gato----\n");
+gato1.mostrarGato();
+
+//Informacion Vaca
+Vaca vaca = new Vaca("Lola", 200, 25);
+Console.WriteLine("\n--------Información Vaca---------");
+vaca.mostrarVaca();
+vaca.CalcularCantidadComida();
+
+Console.WriteLine("\n--------Ballenas-----------");
+Console.Write("Ingresa el nombre de la ballena: ");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Ingresa el peso de la ballena: ");
+peso = double.Parse(Console.ReadLine());
+
+Ballena ballena = new Ballena(nombre, peso);
+
+Console.WriteLine();
+ballena.mostrarballena();
